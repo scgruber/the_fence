e0ff88787acb81d235a0671b582f178c72f6d5d4
Max Hawkins <maxhawkins@gmail.com>
2010-05-16 15:37:03 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0.beta3'
+
+gem "mongoid", "2.0.0.beta4"
+gem "bson_ext", "0.20.1"
+
+gem "haml"
+
+gem "right-rails"
+
+gem 'rspec', ">= 2.0.0.beta.8", :group => :test
+group :test do
+  gem 'webrat'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '78680fc0f212f9eafff91148ad342440b0e9bc0d943541f00f4a04fd3016d32680e9f1690d63ad0aa8068e357a922c920aa2c86a199bfa704892062a0286c45f'
