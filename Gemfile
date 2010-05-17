bf460dda2be72293bec8c336fe5224162787e68b
Max Hawkins <maxhawkins@gmail.com>
2010-05-16 20:12:23 -0400

    ignore .DS_Store

--- Gemfile
+++ Gemfile
@@ -1,11 +1,13 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.0.beta3'
+gem 'rails', :git => "http://github.com/rails/rails.git"
 
 gem "mongoid", "2.0.0.beta4"
 gem "bson_ext", "0.20.1"
 
 gem "haml"
+
+gem "devise", :git => "git://github.com/plataformatec/devise.git"
 
 gem "right-rails"
 
---
+++ config/initializers/devise.rb
@@ -0,0 +1,12 @@
+# Use this hook to configure devise mailer, warden hooks and so forth. The first
+# four configuration values can also be set straight in your models.
+Devise.setup do |config|
+  config.mailer_sender = "admin@takethefence.com"
+
+  config.http_authenticatable = false
+  
+  config.password_length = 6..20
+  config.email_regexp = /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i
+  
+  require 'devise/orm/mongoid'
+end
