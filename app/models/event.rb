acc3f47d61f7084a3632776482c95dcf88cfa047
Max Hawkins <maxhawkins@gmail.com>
2010-05-29 16:43:14 -0400

    Moving image uploader to event file just for now. It makes the web server work.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,22 @@
-require Rails.root + 'app/uploaders/image.rb'
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::RMagick
+
+  storage :grid_fs
+
+  # version :thumb do
+  #   process :scale => [50, 50]
+  # end
+  
+  def default_url
+    "default.png"
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+end
+
 
 class Event
   include Mongoid::Document
--- app/uploaders/image.rb
+++ app/uploaders/image.rb
@@ -1,18 +1,18 @@
-class ImageUploader < CarrierWave::Uploader::Base
-  include CarrierWave::RMagick
-
-  storage :grid_fs
-
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
-  
-  def default_url
-    "default.png"
-  end
-
-  def extension_white_list
-    %w(jpg jpeg gif png)
-  end
-
-end
+# class ImageUploader < CarrierWave::Uploader::Base
+#   include CarrierWave::RMagick
+# 
+#   storage :grid_fs
+# 
+#   # version :thumb do
+#   #   process :scale => [50, 50]
+#   # end
+#   
+#   def default_url
+#     "default.png"
+#   end
+# 
+#   def extension_white_list
+#     %w(jpg jpeg gif png)
+#   end
+# 
+# end
