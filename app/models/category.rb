aa1233377970e378ad57badadf36d764430abb00
Max Hawkins <maxhawkins@gmail.com>
2010-06-20 17:06:44 -0400

    Category unique id should be its name

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category
   include Mongoid::Document
 
+  key :name
+
   field :name, :type => String
   field :kind, :type => String
 
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe Category do
 
+  describe "name" do
+    
+    it "should be id" do
+      category = Factory(:category, :name => "myname")
+      
+      category.id.should == "myname"
+    end
+    
+  end
+
 end
