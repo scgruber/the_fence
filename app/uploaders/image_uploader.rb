b804ffd0d57de9a32ea574349d4f8864e9a235ba
Max Hawkins <maxhawkins@gmail.com>
2010-07-06 23:08:33 -0400

    Added rescaling to images

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,9 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
 
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_fill => [50, 50]
+  end
   
   def default_url
     "default.png"
