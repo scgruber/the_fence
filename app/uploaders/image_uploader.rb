415c721326a33784c2be1aae64e9e128d9637969
Max Hawkins <maxhawkins@gmail.com>
2010-07-06 23:01:59 -0400

    Moved image uploader into its own file (finally)

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,16 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::RMagick
+
+  # version :thumb do
+  #   process :scale => [50, 50]
+  # end
+  
+  def default_url
+    "default.png"
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+end
