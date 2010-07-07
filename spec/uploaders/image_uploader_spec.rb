8c84fa547c1e7b5a43da2088e6eefddfd0245ac7
Max Hawkins <maxhawkins@gmail.com>
2010-07-06 23:20:06 -0400

    Spec for image resizing

--- spec/uploaders/image_uploader_spec.rb
+++ spec/uploaders/image_uploader_spec.rb
@@ -1,7 +1,22 @@
 require 'spec_helper'
 
+require 'carrierwave/test/matchers'
+
 describe ImageUploader do
   
-  it "should have a default file name"
+  before do
+    ImageUploader.enable_processing = true
+    @uploader = ImageUploader.new
+    image_file = File.dirname(__FILE__) + '/../fixtures/poster.gif'
+    @uploader.store!(File.open(image_file))
+  end
+  
+  context "thumb version" do
+    
+    it "should be 50 by 50" do
+      @uploader.thumb.should have_dimensions(50,50)
+    end
+    
+  end
   
 end
