b3d49246c841c340ab4932b838b08fc7544ff63b
Max Hawkins <maxhawkins@gmail.com>
2010-07-11 14:38:27 -0400

    Style for til_whenever

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,5 +1,17 @@
 document.onReady(function(){
   
+  if($('event_til_whenever')) {
+    $('event_til_whenever').onClick(function() {
+      if(this.checked) {
+        $('event_finish').setStyle('color', 'transparent');
+        $('event_finish').disable();
+      } else {
+        $('event_finish').setStyle('color', 'inherit');
+        $('event_finish').enable();
+      }
+    });
+  }
+    
   $$('input.datetime').each(function(datetime){
     new Calendar({format: "%l:%M%p %m/%d/%Y"}).assignTo(datetime);
   });
