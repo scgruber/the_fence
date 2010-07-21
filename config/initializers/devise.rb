cfd0f2a31c66cbf8628f09623b94ad5ad108524e
Max Hawkins <maxhawkins@gmail.com>
2010-07-21 00:33:23 -0400

    Clean scuzz out of devise initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,12 +1,7 @@
-# Use this hook to configure devise mailer, warden hooks and so forth. The first
-# four configuration values can also be set straight in your models.
 Devise.setup do |config|
   config.mailer_sender = "admin@takethefence.com"
 
   config.http_authenticatable = false
-  
-  config.password_length = 6..20
-  config.email_regexp = /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i
 
   config.use_default_scope = true
   config.default_scope = :user
