8ba177d6d85ca358c6e32d1a5595ce309747e00d
Max Hawkins <maxhawkins@gmail.com>
2010-08-08 04:06:50 -0400

    Add JS to hide cost on click free

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -2,17 +2,22 @@
 
 document.onReady(function(){
   
-  if($('event_til_whenever')) {
-    $('event_til_whenever').onClick(function() {
-      if(this.checked) {
-        $('event_finish').setStyle('color', 'transparent');
-        $('event_finish').disable();
-      } else {
-        $('event_finish').setStyle('color', 'inherit');
-        $('event_finish').enable();
-      }
-    });
-  }
+  function toggle_field(field, options) {
+    if($(options.withElement)) {
+      $(options.withElement).onClick(function() {
+        if(this.checked) {
+          $(field).setStyle('color', 'transparent');
+          $(field).disable();
+        } else {
+          $(field).setStyle('color', 'inherit');
+          $(field).enable();
+        }
+      });
+    } 
+  };
+  
+  toggle_field('event_finish', {withElement: 'event_til_whenever'});
+  toggle_field('event_cost', {withElement: 'event_free'});
     
   $$('input.datetime').each(function(datetime){
     var calendar = new Calendar({
