8c0af922ec8f64dd962fce8eb60a1ea459570ade
Max Hawkins <maxhawkins@gmail.com>
2011-01-22 01:02:05 -0500

    Add auth handler for pubcookie

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,20 @@
 class User
   include Mongoid::Document
 
-  devise :omniauthable, :rememberable, :trackable
+  devise :omniauthable, :trackable
          
   has_many_related :events, :foreign_key => :creator_id
   
   field :admin, :type => Boolean, :default => false
+  field :email, :type => String
 
-  def self.find_for_pubcookie_oauth(access_token, signed_in_resource=nil)
-    puts access_token.inspect    
-  end  
+ def self.find_for_pubcookie_oauth(access_token, signed_in_resource=nil)
+  data = access_token['extra']['user_hash']
+  if user = User.find_by_email(data["name"])
+    user
+  else
+    User.create!(:email => data["name"]) 
+  end
+end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -58,7 +58,7 @@ Fence::Application.routes.draw do
   get 'beta' => "beta#info"
   get 'events?sort=upcoming' => 'events#index', :as => 'upcoming_events'
   
-  devise_for :users, :skip => [:sessions] do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }, :skip => [:sessions] do
     get 'login' => 'devise/sessions#new', :as => :new_user_session
     post 'login' => 'devise/sessions#create', :as => :user_session
     get 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
